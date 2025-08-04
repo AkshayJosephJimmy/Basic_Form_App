a7ee9bf1bef44b41989a7c3d3cfd33e5cb9d7165
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-08-04 15:14:16 +0530

    basic header component made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Welcome to the Form App</h1>
       {/* Other components will go here */}
     </div>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { FaRegUserCircle } from "react-icons/fa";
+function Header(){
+    let isLoggedIn = true; // This should be replaced with actual authentication logic
+    return (
+        <div className="flex flex-row justify-between  bg-gray-100 w-full h-20" >
+            <h1 className="text-3xl font-bold underline text-center mt-4">
+                Basic Registration App
+            </h1>
+            <ul className="flex flex-row items-center justify-center space-x-4 mt-4">
+                <li>Home</li>
+                <li>Registration form</li>
+                <li>Admin View</li>
+            </ul>
+            <div>
+
+            <FaRegUserCircle className="h-8 w-8"/>
+            {isLoggedIn?<p>name</p>:<p>Admin Login</p>}
+            </div>
+
+        </div>
+    );
+}
+export default Header;
